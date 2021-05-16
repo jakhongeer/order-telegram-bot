cdc4966b64fec9822de733267a416fdc058dd69e
Jakhongir Nagmatov <jahongirnagmatov@gmail.com>
2021-05-16 23:36:47 +0500

    add .gitignore

--- main.py
+++ main.py
@@ -4,12 +4,17 @@ from config import Api_Token
 
 print("Lezzz go!!..")
 
-
 def start_command(update, context):
     update.message.reply_text("Type something to get started..")
+    context.bot.send_photo(chat_id=chat_id, photo='https://telegra.ph/Hello-world-05-16-17')
+
 
 def help_command(update, context):
-    update.message.reply_text("If you need help, you can ask it on Google!")
+    update.message.reply_text("greet with bot (hi, hello)\nask 'who are you?'\nask a time ('time')")
+
+
+
+
 
 
 def handle_message(update, context):
