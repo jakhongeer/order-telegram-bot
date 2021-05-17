baa1e019a55697ee2150cecd3807ca5db24cd2b4
Jakhongir Nagmatov <jahongirnagmatov@gmail.com>
2021-05-17 11:14:13 +0500

    recreate main and config files

--- main.py
+++ main.py
@@ -1,44 +1,23 @@
-from telegram.ext import *
-from responses import sampleresponses
-from config import Api_Token
+from telegram.ext import Updater, CommandHandler
+from config import API_TOKEN
+import logging
 
-print("Lezzz go!!..")
+
+updater = Updater(token=API_TOKEN, use_context=True)
+
+dp = updater.dispatcher
+
+#It gives you when and why things don't work as expected.
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s' ,
+                    level=logging.DEBUG)
 
 def start_command(update, context):
-    update.message.reply_text("Type something to get started..")
-    context.bot.send_photo(chat_id=chat_id, photo='https://telegra.ph/Hello-world-05-16-17')
-
-
-def help_command(update, context):
-    update.message.reply_text("greet with bot (hi, hello)\nask 'who are you?'\nask a time ('time')")
-
-
-
-
-
-
-def handle_message(update, context):
-    text = str(update.message.text).lower()
-    response = sampleresponses(text)
-
-    update.message.reply_text(response)
-
-def error(update, context):
-    print(f"Update {update} caused error {context.error}!")
-
+    context.bot.send_message(chat_id=update.effective_chat.id, text="Salom men botman. Ko'p ishingizni men orqali avtomatlashtirishingiz mumkin!" )
 
 def main():
-    updater = Updater(Api_Token, use_context=True)
+    start_handler= CommandHandler('start', start_command)
+    dp.add_handler(start_handler)
 
-    dp = updater.dispatcher
-
-    dp.add_handler(CommandHandler("start", start_command))
-    dp.add_handler(CommandHandler("help", help_command))
-
-    dp.add_handler(MessageHandler(Filters.text, handle_message))
-    dp.add_error_handler(error)
     updater.start_polling()
-    updater.idle()
 
 main()
-
