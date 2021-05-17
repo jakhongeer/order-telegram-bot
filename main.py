172f53923b439b98066fd9d5fb78171b59956475
Jakhongir Nagmatov <jahongirnagmatov@gmail.com>
2021-05-18 00:49:17 +0500

    update files

--- main.py
+++ main.py
@@ -9,10 +9,18 @@ dp = updater.dispatcher
 
 #It gives you when and why things don't work as expected.
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s' ,
-                    level=logging.DEBUG)
+                    level=logging.INFO)
 
+# when user sends start command do these
 def start_command(update, context):
-    context.bot.send_message(chat_id=update.effective_chat.id, text="Salom men botman. Ko'p ishingizni men orqali avtomatlashtirishingiz mumkin!" )
+    # For sending a photo to user
+    context.bot.send_photo(chat_id=update.effective_chat.id, photo=open('assets/hello_world.png', 'rb'))
+    # For sending a message
+    context.bot.send_message(chat_id=update.effective_chat.id, text="Hello buddy👋 Welcome to our 'theteam' bot." )
+    # context.bot.send_message(chat_id=update.effective_chat.id, text="Tilni tanlang:\nВыберите язык: \nChoose the language:")
+
+    context.bot.send_message(chat_id=update.effective_chat.id, text="Click OK to continue:")
+
 
 def main():
     start_handler= CommandHandler('start', start_command)
@@ -20,4 +28,3 @@ def main():
 
     updater.start_polling()
 
-main()
